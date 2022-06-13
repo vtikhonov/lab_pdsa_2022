75e8de5084ddca03d6023841f0000e2013da4821
FruteL <kpavel2000@gmail.com>
2022-06-13 11:41:41 +0300

    bad try

---
+++ src/com/Kandieiev/CountMinSketch.java
@@ -0,0 +1,92 @@
+package com.Kandieiev;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import static jdk.nashorn.internal.objects.NativeMath.abs;
+import static jdk.nashorn.internal.objects.NativeMath.min;
+
+public class CountMinSketch {
+
+    String text;
+    private int[] h1;
+    private int[] h2;
+    private int[] h3;
+    private int[] h4;
+    private int[] h5;
+    private int[] h6;
+    private static int size = 11;
+    private int[] a = new int[6];
+    private int[] b = new int[6];
+    Random random = new Random();
+
+    public CountMinSketch() {
+        h1 = new int[size];
+        h2 = new int[size];
+        h3 = new int[size];
+        h4 = new int[size];
+        h5 = new int[size];
+        h6 = new int[size];
+        for (int i = 0; i < 6; i++) {
+            a[i] = random.nextInt(100);
+            b[i] = random.nextInt(100);
+        }
+
+    }
+
+    public void insert(int val)
+    {
+        int hash1 = hashFunction(val, 0);
+        int hash2 = hashFunction(val, 1);
+        int hash3 = hashFunction(val, 2);
+        int hash4 = hashFunction(val, 3);
+        int hash5 = hashFunction(val, 4);
+        int hash6 = hashFunction(val, 5);
+
+        h1[ hash1 ]++;
+        h2[ hash2 ]++;
+        h3[ hash3 ]++;
+        h4[ hash4 ]++;
+        h5[ hash5 ]++;
+        h6[ hash6 ]++;
+    }
+
+    public int hashFunction(int val, int number){
+        return Math.abs(a[number] * val + b[number]) % (size-1);
+    }
+
+    public int sketchCount(int val)
+    {
+        int hash1 = hashFunction(val, 0);
+        int hash2 = hashFunction(val, 1);
+        int hash3 = hashFunction(val, 2);
+        int hash4 = hashFunction(val, 3);
+        int hash5 = hashFunction(val, 4);
+        int hash6 = hashFunction(val, 5);
+        int [] hashes= {hash1, hash2, hash3, hash4, hash5, hash6};
+        int min = Arrays.stream(hashes).min().getAsInt();
+        return min;
+    }
+
+    public void table(){
+        System.out.print("h1 : ");
+        for (int i = 0; i < size; i++)
+            System.out.print(h1[i] +" ");
+        System.out.print("\nh2 : ");
+        for (int i = 0; i < size; i++)
+            System.out.print(h2[i] +" ");
+        System.out.print("\nh3 : ");
+        for (int i = 0; i < size; i++)
+            System.out.print(h3[i] +" ");
+        System.out.print("\nh4 : ");
+        for (int i = 0; i < size; i++)
+            System.out.print(h4[i] +" ");
+        System.out.print("\nh5 : ");
+        for (int i = 0; i < size; i++)
+            System.out.print(h5[i] +" ");
+        System.out.print("\nh6 : ");
+        for (int i = 0; i < size; i++)
+            System.out.print(h6[i] +" ");
+        System.out.println("");
+    }
+}
--- src/com/Kandieiev/Main.java
+++ src/com/Kandieiev/Main.java
@@ -4,20 +4,22 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String text = readFile(args[0]);
+        String text = readFile("short.txt");
         text = text.toLowerCase(Locale.ROOT);
         text = text.replaceAll("[^A-Za-z ]", "");
-        text = deleteWords(args[1], text);
+        text = deleteWords("skip_words.txt", text);
         System.out.println(text);
 
+        CountMinSketch cms = new CountMinSketch();
+
+        Arrays.stream(text.split(" ")).forEach(e -> cms.insert(e.hashCode()));
+        cms.table();
+        System.out.println("Min = " + cms.sketchCount("adventure".hashCode()));
     }
 
     public static String readFile(String path) throws IOException {
@@ -36,5 +38,4 @@ public class Main {
         }
         return text;
     }
-
 }
